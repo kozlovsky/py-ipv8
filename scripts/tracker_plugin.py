b6ae541e29375b2b0e712bbd68062b94bfdc0ce2
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2020-10-06 10:00:56 +0200

    Use sliding HyperLogLog to collect data. Report to InfluxDB and collector service

--- scripts/tracker_plugin.py
+++ scripts/tracker_plugin.py
@@ -4,11 +4,19 @@ This script enables to start the tracker.
 Select the port you want to use by setting the `listen_port` command line argument.
 """
 import argparse
+import base64
+from binascii import unhexlify
+from collections import defaultdict
+import datetime
+import json
 import logging
 import random
+import requests
 import signal
 import sys
 import time
+import traceback
+import zlib
 from asyncio import ensure_future, get_event_loop, sleep
 
 # Check if we are running from the root directory
@@ -28,8 +36,22 @@ from ipv8.peer import Peer
 from ipv8.peerdiscovery.churn import DiscoveryStrategy
 from ipv8.peerdiscovery.network import Network
 
+from hyperloglog import SlidingHyperLogLog as SHLL
+
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+POPULARITY_COMMUNITY_PREFIX = unhexlify(b'00029aca62f878969c437da9844cba29a134917e1648')
+
+COLLECTOR_URL = 'http://localhost:3322/bootstrap/report/'
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+
+
+def new_counter() -> SHLL:
+    return SHLL(0.01, HOUR)
 
 
 class SimpleChurn(DiscoveryStrategy):
@@ -55,12 +77,54 @@ class EndpointServer(Community):
     """
     master_peer = Peer(default_eccrypto.generate_key(u"very-low"))
 
-    def __init__(self, endpoint):
+    def __init__(self, endpoint, listen_port):
         my_peer = Peer(default_eccrypto.generate_key(u"very-low"))
         self.signature_length = default_eccrypto.get_signature_length(my_peer.public_key)
         super(EndpointServer, self).__init__(my_peer, endpoint, Network())
         self.churn_strategy = SimpleChurn(self)
         self.churn_task = self.register_task("churn", self.churn_strategy.take_step, interval=30)
+        self.report_task = self.register_task("stats_report", self.stats_report, interval=15)
+
+        self.listen_port = listen_port
+        self.all_addresses = new_counter()
+        self.all_peers = new_counter()
+        self.popularity_addresses = new_counter()
+        self.popularity_peers = new_counter()
+
+    def record_peer(self, peer, address, prefix):
+        t = time.time()
+        self.all_addresses.add(t, address)
+        self.all_peers.add(t, peer.mid)
+        if prefix == POPULARITY_COMMUNITY_PREFIX:
+            self.popularity_addresses.add(t, address)
+            self.popularity_peers.add(t, peer.mid)
+            print('total address count:', self.all_addresses.card(t))
+
+    def stats_report(self):
+        t = time.time()
+
+        print('DEBUG')
+        self.all_addresses.add(t, str(random.random()))
+        self.all_peers.add(t, str(random.random()))
+        self.all_peers.add(t, str(random.random()))
+
+        data = {
+            'addresses': self.all_addresses.LPFM,
+            'peers': self.all_peers.LPFM,
+            'popularity_addresses': self.popularity_addresses.LPFM,
+            'popularity_peers': self.popularity_peers.LPFM
+        }
+        s = json.dumps(data)
+        b = base64.b64encode(zlib.compress(s.encode('utf-8'))).decode('ascii')
+        print(f'Preparing data. Raw JSON size: {len(s)}, compressed size: {len(b)}')
+        print('posting data to collector')
+        try:
+            requests.post(COLLECTOR_URL, json={
+                'port': self.listen_port,
+                'compressed_data': b
+            })
+        except Exception:
+            traceback.print_exc()
 
     def on_packet(self, packet, warn_unknown=False):
         source_address, data = packet
@@ -73,7 +137,6 @@ class EndpointServer(Community):
             elif warn_unknown:
                 self.logger.warning("Tracker received unknown message %s", str(data[22]))
         except Exception:
-            import traceback
             traceback.print_exc()
 
     def on_generic_introduction_request(self, source_address, data, prefix):
@@ -81,10 +144,13 @@ class EndpointServer(Community):
         peer = Peer(auth.public_key_bin, source_address)
         peer.last_response = time.time()
 
-        self.network.add_verified_peer(peer)
-        self.network.discover_services(peer, [prefix[2:], ])
+        self.record_peer(peer, source_address, prefix)
 
-        intro_peers = [p for p in self.network.get_peers_for_service(prefix[2:]) if not(p == peer)]
+        service_id = prefix[2:]
+        self.network.add_verified_peer(peer)
+        self.network.discover_services(peer, [service_id])
+
+        intro_peers = [p for p in self.network.get_peers_for_service(service_id) if not(p == peer)]
         if intro_peers:
             intro_peer = random.choice(intro_peers)
         else:
@@ -119,7 +185,7 @@ class TrackerService(object):
         """
         self.endpoint = UDPEndpoint(listen_port)
         await self.endpoint.open()
-        self.overlay = EndpointServer(self.endpoint)
+        self.overlay = EndpointServer(self.endpoint, listen_port)
 
         async def signal_handler(sig):
             print("Received shut down signal %s" % sig)
